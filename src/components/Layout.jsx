1e559d462f4d5a7f10286d8aff2d9429c8071699
mayankinnodem <uska-email>
2026-07-31 16:22:39 +0530

    sdcdsc

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 
-import Navbar from './../components/Navbar';
+import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
